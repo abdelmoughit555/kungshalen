a3156c4f98a19d13c034c12ac707087ebbb04fc2
abdelmoughit555 <fouhamtrance@gmail.com>
2020-03-21 20:55:43 +0000

    create cart table for visitors to store items

---
+++ database/migrations/2020_03_21_202808_create_cart_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCartTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('visitor_id')->unsigned()->index();
+            $table->bigInteger('product_id')->unsigned()->index();
+
+            $table->bigInteger('quantity')->default(1);
+            $table->integer('price');
+
+            $table->foreign('visitor_id')->references('id')->on('visitors');
+            $table->foreign('product_id')->references('id')->on('products');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart');
+    }
+}
